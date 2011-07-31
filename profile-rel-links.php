5ec32895a0dee4b85b50d30c24a535e48b9b592c
Aria Stewart <aredridel@nbtsc.org>
2011-07-31 11:38:32 -0600

    Add plugin comment header

--- profile-rel-links.php
+++ profile-rel-links.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * @package Profile rel= links
+ * @version 1.0
+ * @requires php >= 5.3
+ */
+/*
+Plugin Name: profile-rel-links
+Plugin URI: http://github.com/blumenthals/profile-rel-links
+Description: Adds rel= links to profiles for matching up with Twitter and Google profiles
+Author: Aria Stewart <aria@blumenthals.com>
+Version: 1.0
+Author URI: http://blumenthals.com/
+*/
 
 add_action('init', function() {
 
